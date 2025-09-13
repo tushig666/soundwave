1bddfaa859bd1e19550c62982b6bae94ad12c35c
tushig tse <tsetushig@gmail.com>
2025-09-13 02:29:06 +0000

    za odoo jishee ymar ch hamaagui hugjim togluulmaar baina , mun bairshuul

--- next.config.ts
+++ next.config.ts
@@ -13,18 +13,6 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
         hostname: 'picsum.photos',
         port: '',
         pathname: '/**',
